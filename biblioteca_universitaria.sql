8f53f0817029f99d06196542227f242e92f74ad2
João Vitor <63158937+JhonW67@users.noreply.github.com>
2025-06-10 16:15:05 -0400

    Create biblioteca_universitaria.sql
    inicio banco de dados

---
+++ biblioteca_universitaria.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE biblioteca_universitaria;
+USE biblioteca_universitaria;
+CREATE TABLE alunos (
+    id_aluno INT AUTO_INCREMENT PRIMARY KEY
+    ,nome VARCHAR(100) NOT NULL
+    ,curso VARCHAR(100)
+    ,data_matricula DATE NOT NULL
+);
+CREATE TABLE livros (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY
+    ,titulo VARCHAR(200) NOT NULL
+    ,autor VARCHAR(100) NOT NULL
+    ,quantidade_estoque INT NOT NULL CHECK (quantidade_estoque >= 0)
+);
+CREATE TABLE emprestimos (
+    id_emprestimo INT AUTO_INCREMENT PRIMARY KEY
+    ,id_aluno INT
+    ,id_livro INT
+    ,data_emprestimo DATE NOT NULL
+    ,data_devolucao DATE DEFAULT NULL 
+); 
+CREATE TABLE penalidades (
+    id_penalidade INT AUTO_INCREMENT PRIMARY KEY
+    ,id_aluno INT
+    ,descricao TEXT NOT NULL
+    ,data_penalidade DATE NOT NULL
+);
